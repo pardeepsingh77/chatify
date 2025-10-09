b2b2d38fcac64c7bbe34716f52400e4a64c059b0
Pardeep singh <ps5380028@gmail.com>
2025-10-10 00:42:45 +0530

    sign up controller add

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv'
 import path from 'path'
 import authRoutes from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
+import { connectDB } from '../lib/db.js';
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000
+
+app.use(express.json()); // req.body
 
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoute)
@@ -19,4 +22,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`))
+app.listen(PORT,()=> {
+    console.log(`Server is running on port ${PORT}`) 
+    connectDB();
+})
