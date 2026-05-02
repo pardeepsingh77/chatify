1c0cfb28899657edf15370c28454d83c7dc79a61
Developer inizia.agency <developer@inizia.agency>
2026-05-03 00:01:56 +0530

    messages

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,87 @@
+import cloudinary from '../../lib/cloudinary.js';
+import Message from '../modals/message.modal.js'
+import User from '../modals/user.modal.js'
+
+export const getAllContacts = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const filteredUsers = await User.find({ _id: { $ne: userId } }).select('-password');
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log("Error in getAllContacts controller", error)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const getMessagesByUserId = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const otherUserId = req.params.id;
+        const messages = await Message.find({
+            $or: [
+                { senderId: userId, receiverId: otherUserId },
+                { senderId: otherUserId, receiverId: userId }
+            ]
+        }).sort({ createdAt: 1 })
+        res.status(200).json(messages)
+    }
+    catch (err) {
+        console.log("Error in getMessagesByUserId controller", err)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+        if(!text && !image){
+            return res.status(400).json({message : "Message text or image is required"})
+        }
+        if(senderId.equals(receiverId)){
+            return res.status(400).json({message : "You cannot send message to yourself"})
+        }
+        const receiverExists = await User.findById(receiverId);
+        if(!receiverExists){
+            return res.status(404).json({message : "Receiver not found"})
+        }
+        let imageUrl; 
+        if(image){
+            const data = await cloudinary.uploader.upload(image);
+            imageUrl = data.secure_url
+        }
+        const message = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+        await message.save();
+        res.status(201).json({newMessage:message})
+    } catch (error) {
+        console.log("Error in sendMessage controller", error)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const getAllChats = async (req , res) => {
+    try {
+        const loggedInUser = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                {senderId : loggedInUser},
+                {receiverId : loggedInUser}
+            ]
+        })
+        const chatPartnerIds =[ ...new Set(
+            messages.map((msg)=>msg.senderId.toString() === loggedInUser.toString() ? msg.receiverId.toString() : msg.senderId.toString())
+        )]
+        const chatPartners = await User.find({_id : {$in : chatPartnerIds}}).select("-password");
+        res.status(200).json(chatPartners)
+    } catch (error) {
+        console.log("Error in getAllChats controller", error)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import { protectRoute } from '../middlewares/auth.middleware.js';
+import { getAllChats, getAllContacts, getMessagesByUserId, sendMessage } from '../controllers/message.controller.js';
+import { arcjetProtection } from '../middlewares/arcjet.middleware.js';
 
 const router = express.Router();
 
-router.get('/send',(req,res)=>{
-    res.send('Send Message Endpoint')
-})
+router.use(arcjetProtection,protectRoute)
+
+router.get('/contacts',getAllContacts)
+router.get('/chats',getAllChats)
+router.get('/:id',getMessagesByUserId)
+router.get('/send/:id',sendMessage)
 
 export default router;
