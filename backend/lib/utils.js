a63e39206cb221783433ac9fcdbb0323b0db7e1e
Developer inizia.agency <developer@inizia.agency>
2026-05-16 19:35:56 +0530

    change

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -9,11 +9,13 @@ export const generateToken = (userId , res) => {
         expiresIn: "7d"
     });
 
+    const isProduction = ENV.NODE_ENV === 'production';
+
     res.cookie("jwt",token,{
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly : true, // prevent XSS attacks : cross-site scripting
-        sameSite : "strict", // CRSF attacks
-        secure : ENV.NODE_ENV === 'development' ? false : true,
+        sameSite : isProduction ? "none" : "lax",
+        secure : isProduction,
     })
     
     return token;
